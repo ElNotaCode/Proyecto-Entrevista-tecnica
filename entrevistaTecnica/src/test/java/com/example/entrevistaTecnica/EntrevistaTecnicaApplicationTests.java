0da7e982e038d649acbc7ac1a66df061ecb6e989
Marc Lopez <marclopezmarco469@gmail.com>
2022-05-20 10:30:01 +0200

    Proyecto Solo con Dependencias

---
+++ entrevistaTecnica/src/test/java/com/example/entrevistaTecnica/EntrevistaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entrevistaTecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrevistaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

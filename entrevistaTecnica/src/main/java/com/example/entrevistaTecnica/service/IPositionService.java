9b6f2469754b5d3ece5c9b1c496595b751789f99
Marc Lopez <marclopezmarco469@gmail.com>
2022-05-21 11:10:54 +0200

    Acabado

---
+++ entrevistaTecnica/src/main/java/com/example/entrevistaTecnica/service/IPositionService.java
@@ -0,0 +1,22 @@
+package com.example.entrevistaTecnica.service;
+
+import java.util.List;
+
+import com.example.entrevistaTecnica.dto.Position;
+
+
+public interface IPositionService {
+	
+	
+	//Metodos del CRUD
+			public List<Position> listarPositions(); //Listar All 
+			
+			public Position guardarPosition(Position position);	//Guarda un Position CREATE
+			
+			public Position positionXID(int id); //Leer datos de un Position READ
+			
+			public Position actualizarPosition(Position position); //Actualiza datos del Position UPDATE
+			
+			public void eliminarPosition(int id);// Elimina el Position DELETE
+
+}

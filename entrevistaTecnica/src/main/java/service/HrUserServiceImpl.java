30d84f9099c69e1c4610996dca84b09b2a634fdf
Eloi Martorell Martín <eloi.martin174@gmail.com>
2022-05-23 21:22:32 +0200

    HrUser Done

---
+++ entrevistaTecnica/src/main/java/service/HrUserServiceImpl.java
@@ -0,0 +1,55 @@
+package service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dao.IHrUserDAO;
+import dto.HrUser;
+
+/**
+ * 
+ * @author Eloi Martorell Martín
+ *
+ */
+
+@Service
+public class HrUserServiceImpl implements IHrUserService{
+
+	@Autowired
+	IHrUserDAO iHrUserDAO;
+	
+	@Override
+	public List<HrUser> listarHrUser() 
+	{
+		return iHrUserDAO.findAll();
+	}
+
+	@Override
+	public HrUser createHrUser(HrUser hrUser) 
+	{
+		return iHrUserDAO.save(hrUser);
+	}
+
+	@Override
+	public HrUser readHrUser(int id) 
+	{
+		return iHrUserDAO.findById(id).get();
+	}
+
+	@Override
+	public HrUser updateHrUser(HrUser hrUser) 
+	{
+		return iHrUserDAO.save(hrUser);
+	}
+
+	@Override
+	public void deleteHrUser(int id) 
+	{
+		iHrUserDAO.deleteById(id);
+	}
+	
+	
+
+}

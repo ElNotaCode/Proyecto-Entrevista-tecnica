784d4dce213fe0acc6105828a6c94549a2ff7bd9
Borja Montseny <68342939+borjaMontseny@users.noreply.github.com>
2022-05-25 20:30:16 +0200

    commit 25/05

--- entrevistaTecnica/src/main/java/com/example/entrevistaTecnica/controller/CandidateController.java
+++ entrevistaTecnica/src/main/java/com/example/entrevistaTecnica/controller/CandidateController.java
@@ -18,19 +18,50 @@ import com.example.entrevistaTecnica.dto.Candidate;
 @RestController
 @RequestMapping("/api")
 public class CandidateController {
-	
+
 	@Autowired
 	CandidateServiceImpl candidateServiceImpl;
-	
-	@GetMapping("/cursos")
-	public List<Candidate> listarCandidate(){
+
+	@GetMapping("/candidates")
+	public List<Candidate> listarCandidate() {
 		return candidateServiceImpl.listarCandidates();
 	}
-	
+
 	@PostMapping("/candidates")
 	public Candidate salvarCandidate(@RequestBody Candidate candidate) {
-		
+
 		return candidateServiceImpl.guardarCandidate(candidate);
 	}
-	
+
+	@GetMapping("/candidates/{id}")
+	public Candidate CandidateXID(@PathVariable(name = "id") int id) {
+
+		Candidate Candidate_xid = new Candidate();
+
+		Candidate_xid = candidateServiceImpl.candidateXID(id);
+
+		return Candidate_xid;
+	}
+
+	@PutMapping("/candidates/{id}")
+	public Candidate actualizarCandidate(@PathVariable(name = "id") int id, @RequestBody Candidate candidate) {
+
+		Candidate Candidate_seleccionado = new Candidate();
+		Candidate Candidate_actualizado = new Candidate();
+
+		Candidate_seleccionado = candidateServiceImpl.candidateXID(id);
+
+		Candidate_seleccionado.setCandidate_name(candidate.getCandidate_name());
+		Candidate_seleccionado.setCandidate_surname(candidate.getCandidate_surname());
+
+		Candidate_actualizado = candidateServiceImpl.actualizarCandidate(Candidate_seleccionado);
+
+		return Candidate_actualizado;
+	}
+
+	@DeleteMapping("/registroCursos/{id}")
+	public void eleiminarCandidate(@PathVariable(name = "id") int id) {
+		candidateServiceImpl.eliminarCandidate(id);
+	}
+
 }

0c31517dea28c72b3d8930b620a049f5631ed199
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-06-28 10:02:53 +0200

    crossOrigin añadido

--- entrevistaTecnica/src/main/java/com/example/entrevistaTecnica/controller/CandidateController.java
+++ entrevistaTecnica/src/main/java/com/example/entrevistaTecnica/controller/CandidateController.java
@@ -3,6 +3,7 @@ package com.example.entrevistaTecnica.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.entrevistaTecnica.service.CandidateServiceImpl;
 import com.example.entrevistaTecnica.dto.Candidate;
+import com.example.entrevistaTecnica.service.CandidateServiceImpl;
 
 /**
  * 
@@ -23,6 +25,7 @@ import com.example.entrevistaTecnica.dto.Candidate;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
 public class CandidateController {
 
 	@Autowired
--- entrevistaTecnica/src/main/java/com/example/entrevistaTecnica/controller/SkillController.java
+++ entrevistaTecnica/src/main/java/com/example/entrevistaTecnica/controller/SkillController.java
@@ -7,6 +7,7 @@ package com.example.entrevistaTecnica.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entrevistaTecnica.dto.Skill;
@@ -21,6 +23,7 @@ import com.example.entrevistaTecnica.service.SkillServiceImpl;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
 public class SkillController {
 
 	@Autowired
